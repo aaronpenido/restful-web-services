8a67ce2570eb21798184794afbe7344f748627b0
Aaron Penido <aaron.penido@gmail.com>
2018-02-01 19:35:05 -0200

    Create methods to get all users, a user by id and create a new one.

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/UserResource.java
@@ -0,0 +1,43 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import com.in28minutes.rest.webservices.restfulwebservices.dao.UserDaoService;
+import com.in28minutes.rest.webservices.restfulwebservices.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    private List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping(value = "/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
